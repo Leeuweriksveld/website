10efc90d849df8f2ab0c28fcfc8a3c9e5db722bc
Nathalie van Hooff <Nathalie1@MacBook-Pro-van-Nathalie-3.local>
2026-07-05 09:49:14 +0200

    Fix base-pad voor GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://www.leeuweriksveld.nl',
+  site: 'https://leeuweriksveld.github.io',
+  base: '/website',
 });
